b5306de9ee91bd9430f782ac7f5043848b9795eb
saadcs7 <90066565+saadcs7@users.noreply.github.com>
2021-09-06 14:17:18 +0400

    Update cities-endpoint.service.ts

--- src/app/services/cities/cities-endpoint.service.ts
+++ src/app/services/cities/cities-endpoint.service.ts
@@ -2,20 +2,19 @@ import { Injectable, Injector } from "@angular/core";
 import { BaseService } from "../base.service";
 import { Observable } from "rxjs/Observable";
 import { HttpClient } from "@angular/common/http";
-import { ICity } from "../../models/city.model";
 
 @Injectable()
 export class CitiesEndpoint extends BaseService {
     constructor(protected _httpClient: HttpClient, private _injector: Injector) {
         super(_httpClient, _injector);
     }
-    getCities(): Observable<ICity[]> {
-        return this.Http.get<ICity[]>(this.BaseUrl + 'cities');
+    getCities<city>(): Observable<city[]> {
+        return this.Http.get<city[]>(this.BaseUrl + 'cities');
     }
     deleteCity(id: number){
         return this.Http.delete(this.BaseUrl + 'cities/delete-city/' + id);
     }
-    async getCity(id: number): Promise<ICity> {
-        return await this.Http.get<ICity>(this.BaseUrl+ 'cities/' + id).toPromise();
+    async getCity<city>(id: number): Promise<city> {
+        return await this.Http.get<city>(this.BaseUrl+ 'cities/' + id).toPromise();
     }
 }
